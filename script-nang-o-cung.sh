06c0a3ec924fdb5ff2d97d5af2c7a7b9a59d5bde
DESKTOP-2Q576RM\admin <bachdangtuan.dev@gmail.com>
2023-11-23 22:14:11 +0700

    update lại source

--- script-nang-o-cung.sh
+++ script-nang-o-cung.sh
@@ -1,46 +1,73 @@
-##### Variable List
-arrayMenu=("T·∫°o ph√¢n v√πng m∆°i" "N√¢ng dung l∆∞·ª£ng ·ªï c·ª©ng" "Tho√°t")
+##### Variable List ######333
+arrayMenu=("T?o ph‚n v˘ng m?i" "N‚ng dung l??ng ? c?ng" "Tho·t")
 mapfile -t vg_names < <(vgdisplay | awk '/VG Name/ {print $3}')
+capacityNumber=""
 
-# Danh s√°ch menu
+# Danh s·ch menu
 function list_menu() {
     for ((i = 0; i < ${#arrayMenu[@]}; i++)); do
-        echo "                             $((i + 1)). ${arrayMenu[$i]}                          "
+        echo "                                 $((i + 1)). ${arrayMenu[$i]}                          "
     done
 }
-# L·∫•y th√¥ng tin ·ªï disk
+# L?y thÙng tin ? disk
 function lay_thong_tin_disk() {
     clear
     echo " "
-    echo "============================ Check th√¥ng tin ·ªï ƒëƒ©a df -HT ==================================="
+    echo "============================ Check thÙng tin ? ??a df -HT ==================================="
     df -hT
     echo " "
-    echo "============================ Check th√¥ng tin ·ªï ƒëƒ©a lsblk ============================"
+    echo "============================ Check thÙng tin ? ??a lsblk ============================"
     lsblk
 }
 
-# T·∫°o ph√¢n v√πng
-
+# T?o ph‚n v˘ng
 function create_partition_disk() {
-    fdisk "/dev/$1" <<EOF
-    n
-    p
+    nameDisk=$1
+    capacityDisk=$2
+    partitionNumber=$3
 
+    echo "TÍn disk: $nameDisk"
+    echo "Kh?i l??ng: $capacityDisk"
+    echo "Partition : $partitionNumber"
 
-    +2G
-    t
+    if [ -n "$3" ]; then
+        echo "ok"
+        fdisk "/dev/$1" <<EOF
+        n
+        p
+        $partitionNumber
 
-    8e
-    w
+        +${capacityDisk}G
+        t
+
+        8e
+        w   
 EOF
-    echo "T·∫†O PARTITION TH√ÄNH C√îNG"
+        echo "T?O PARTITION TH¿NH C‘NG"
+    else
+        echo "ko ok l?m"
+        fdisk "/dev/$1" <<EOF
+        n
+        p
+        $partitionNumber
+
+        +${capacityDisk}G
+        t
+
+        8e
+        w   
+EOF
+
+        echo "T?O PARTITION TH¿NH C‘NG"
+    fi
+
 }
 
-# T·∫°o volume group
+# T?o volume group
 function create_volume_group() {
-    read -p "Nh·∫≠p t√™n volume-group mu·ªën t·∫°o : " nameOfVolumeGroup
+    read -p "Nh?p tÍn volume-group mu?n t?o : " nameOfVolumeGroup
     # Check volume group
-    # echo " Danh s√°ch volume group:  ${vg_names[@]}  "
+    # echo " Danh s·ch volume group:  ${vg_names[@]}  "
     for vg_name in "${vg_names[@]}"; do
         if [ "$vg_name" == "$nameOfVolumeGroup" ]; then
             found=true
@@ -48,60 +75,133 @@ function create_volume_group() {
         fi
     done
     if [ "$found" == true ]; then
-        echo "T√™n $nameOfVolumeGroup ƒë√£ ƒë∆∞·ª£c s·ª≠ d·ª•ng"
+        echo "TÍn $nameOfVolumeGroup ?„ ???c s? d?ng"
     else
-        echo "T√™n $nameOfVolumeGroup c√≥ th·ªÉ s·ª≠ d·ª•ng"
-        read -p "Nh·∫≠p t√™n ph√¢n v√πng mu·ªën t·∫°o (nh·∫≠p d·∫°ng sdbx) : " partition_number
+        echo "TÍn $nameOfVolumeGroup cÛ th? s? d?ng"
+        read -p "Nh?p tÍn ph‚n v˘ng mu?n t?o (nh?p d?ng sdbx) : " partition_number
         vgcreate $nameOfVolumeGroup /dev/$partition_number
     fi
 }
 
-# Main Function
+# ??ng ˝ t?o ? c?ng
+function accept_create() {
+    read -p "B?n cÛ ??ng ˝ t?o ? c?ng khÙng? (y/n): " choice
+    case $choice in
+    [yY])
+        while true; do
+            read -p "Nh?p s? ph‚n v˘ng, ch? nh?p s? khÙng c?n nh?p tÍn sdX, khÙng nh?p gÏ l‡ m?c ??nh theo th? t? (vÌ d?: $nameOFdisk 1, $nameOFdisk 2,..): " partitionNumber
+
+              if ls /dev | grep -q $nameOFdisk$partitionNumber && [ -n "$partitionNumber" ]; then
+
+
+                echo "?„ cÛ ? c?ng n‡y r˘i"
+            else
+
+                # Check s?
+                function is_number() {
+                    [[ $1 =~ ^[0-9]+$ ]]
+                }
+
+                while true; do
+                    read -p "Nh?p dung l??ng tÌnh theo GB: " capacityNumber
+
+                    if is_number "$capacityNumber"; then
+                        break
+                    else
+                        echo "Vui lÚng ch? nh?p s?."
+                    fi
+                done
+
+                if [ -n "$partitionNumber" ]; then
+                    # ??y config theo s? ???c nh?p
+                    create_partition_disk $nameOFdisk $capacityNumber $partitionNumber
+                    echo " "
+                    echo "Danh S·ch ph‚n v˘ng lsblk m?i"
+                    echo " "
+                    lsblk
+                    # create_volume_group
+                else
+                    # ??y m?c ??nh s? th? t? d?ng sdb1 sdb2 sdb3
+                    create_partition_disk $nameOFdisk $capacityNumber $partitionNumber
+                    echo " "
+                    echo "Danh S·ch ph‚n v˘ng lsblk m?i"
+                    echo " "
+                    lsblk
+                    # create_volume_group
+                fi
+
+            fi
+        done
+        ;;
+
+    [nN])
+        exit
+        ;;
+    esac
+}
+
+# MAIN FUNCTIONS
 function tao_sdxY() {
     echo " "
-    read -p "Nh·∫≠p t√™n ·ªï ƒëƒ©a mu·ªën x·ª≠ l√Ω (·ªï disk c√≥ d·∫°ng sdX) : " nameOFdisk
-    # Ki·ªÉm tra k√Ω t·ª± nh·∫≠p v√†o n·∫øu > 3 th√¨ b√°o nh·∫≠p sai ki·ªÉu t√™n ·ªï c·ª©ng
+    read -p "Nh?p tÍn ? ??a mu?n x? l˝ (? disk cÛ d?ng sdX) : " nameOFdisk
+    # Ki?m tra k˝ t? nh?p v‡o n?u > 3 thÏ b·o nh?p sai ki?u tÍn ? c?ng
     if [ ${#nameOFdisk} -gt 3 ]; then
-        echo "nh·∫≠p sai t√™n ·ªï c·ª©ng l√†m g√¨ c√≥ ·ªï c·ª©ng n√†o t√™n l√† $nameOFdisk, t·ª± tho√°t sau 3s "
+        echo "nh?p sai tÍn ? c?ng l‡m gÏ cÛ ? c?ng n‡o tÍn l‡ $nameOFdisk, t? tho·t sau 3s "
         sleep 3
         exit
     else
         if ls /dev | grep -q $nameOFdisk; then
-            # T·ªìn t·∫°i ·ªï c·ª©ng th√¨ t·∫°o ti·∫øp t·ª•c check dung l∆∞·ª£ng
-
+            # T?n t?i ? c?ng thÏ t?o ti?p t?c check dung l??ng
             mapfile -t size_free_space < <(parted /dev/$nameOFdisk unit s print free | awk '/Free Space/ {print $3}' | sed 's/s$//')
-            disk_space_sector_default= "${size_free_space[0]}"
-            disk_space_sector="${size_free_space[1]}"
-            echo "Sector c√≤n l√† $disk_space_sector"
 
-            if [ [ "$disk_space_sector" ] > 2000 || [ "$disk_space_sector_default" ] = 1985 ]; then
-                echo "Dung l∆∞·ª£ng c√≤n"
-                # create_partition_disk $nameOFdisk
-                echo " "
-                echo " Danh S√°ch ph√¢n v√πng lsblk m·ªõi"
-                echo " "
-                lsblk
-                create_volume_group
-            else
-                echo "H·∫øt dung l∆∞·ª£ng "
-                exit
-            fi
+            array_length=${#size_free_space[@]}
+
+            disk_space_sector_default="${size_free_space[0]}"
+            disk_space_sector="${size_free_space[1]}"
+
+            # echo "?? d‡i m?ng size_free_space ${#size_free_space[@]}"
+            case $array_length in
+            1)
+                if [ "$disk_space_sector_default" -gt 5000 ]; then
+                    echo "Dung l??ng cÚn, b?n cÛ th? t?o ? c?ng"
+                    accept_create
+                else
+                    echo "H?t dung l??ng"
+                    exit
+                fi
+                ;;
+
+            2 | *)
+
+                if [ "$disk_space_sector" -gt 5000 ]; then
+                    echo "Dung l??ng cÚn, b?n cÛ th? t?o ? c?ng"
+                    accept_create
+
+                else
+                    echo "H?t dung l??ng"
+                    exit
+                fi
+                ;;
+            esac
 
         else
-            # Kh√¥ng t·ªìn t·∫°i
-            echo "Kh√¥ng c√≥ ·ªï c·ª©ng n√†o l√† $nameOFdisk"
+            # KhÙng t?n t?i
+            echo "KhÙng cÛ ? c?ng n‡o l‡ $nameOFdisk"
         fi
     fi
 
 }
 
+# MENU FUNCTION
 echo "---------------------------------------------------------------------------------"
-echo "----                                N√¢ng c·∫•p ·ªï c·ª©ng                          ----"
+echo "---------------------------------------------------------------------------------"
+echo "----                                N‚ng c?p ? c?ng                          ----"
 echo "----                     Distribution Linux: Ubuntu/RHEL/CentOS              ----"
-echo "----                                  ********                               ----"
+echo "----                                  **ver1.1**                           ----"
 list_menu
 echo "---------------------------------------------------------------------------------"
-read -p "Vui l√≤ng ch·ªçn t·ª´ [1-${#menu[@]}]: " choice
+echo "---------------------------------------------------------------------------------"
+read -p "Vui lÚng ch?n t? [1-${#arrayMenu[@]}]: " choice
 case $choice in
 1)
     clear
