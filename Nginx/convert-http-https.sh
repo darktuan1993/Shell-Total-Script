6fb024fc45d71e0a8e06642d1df755f7bed484d2
OFFICEVNPAY\tuanbd <bachdangtuan.dev@gmail.com>
2023-12-05 13:09:21 +0700

    update config

--- Nginx/convert-http-https.sh
+++ Nginx/convert-http-https.sh
@@ -10,16 +10,17 @@
 
 # Variable
 ssl_config="
-    ssl_certificate /etc/nginx/vhosts/cert/test.cer;
-    ssl_certificate_key /etc/nginx/vhosts/cert/test.key;
-    ssl_dhparam /etc/nginx/vhosts/cert/test.pem;
-    ssl_trusted_certificate /etc/nginx/vhosts/cert/test.pem;
+
+    ssl_certificate /etc/nginx/cert_vnpay_local/vnpay.local.pem;
+    ssl_certificate_key /etc/nginx/cert_vnpay_local/vnpay.local.key;
+    ssl_dhparam /etc/nginx/cert_vnpay_local/dhparam.pem;
+
 "
     host_var="\$host"
     request_uri_var="\$request_uri"
     path_config="/etc/nginx/conf.d/*.conf"
 
-# Thêm khối code server và add thêm config 
+# Thêm khối code server và add thêm config
 function add_line_config {
 
     # Xác định vị trí dòng của directive server tokens
@@ -34,7 +35,7 @@ function add_line_config {
         awk -v line="$insert_line" -v config="$ssl_config" 'NR == line {print config} {print}' "$1" > "$1.tmp" && mv "$1.tmp" "$1"
     else
         # Giảm số dòng lên 1 để xác định vị trí thêm cấu hình mới và Thêm cấu hình SSL vào vị trí ngay sau dòng location /;
-        echo "Add con fix vao truoc dong location / trong $1"
+        echo "Add con fix vao duoi dong location / trong $1"
         insert_line=$((line_number_location - 1))
         awk -v line="$insert_line" -v config="$ssl_config" 'NR == line {print config} {print}' "$1" > "$1.tmp" && mv "$1.tmp" "$1"
     fi
@@ -57,29 +58,20 @@ server {
 
     # Update config
 function update_config {
-        for file in /etc/nginx/conf.d/*.conf
-        do
-            filename="${file##*/}"  # Bỏ phần đường dẫn
-            filename="${filename%.conf}"  # Bỏ phần đuôi .conf
-            listen_directive=$(awk '/^server\s*{/,/^}/ {if ($1 == "listen") print $2}' "$file")
 
-            # Kiểm tra xem "443" có xuất hiện trong chuỗi "listen_directive" hay không
-            if [[ $listen_directive == "443" ]]; then
-                echo "Da cau hinh SSL"
-                # Nếu gặp 443 sẽ ngắt luôn không cấu hình nữa
-                break;  
-                exit;
-            else
-                echo "File config chua cau hinh SSL "
-                # Sửa listen thành 443 và sll 
-                sed -i "s/$listen_directive/443 ssl/g" "$file"
-                add_line_config $file
-                # cat $file
-
-            fi
-        done
-
+    for file in /etc/nginx/vhosts/*.conf
+    do
+        filename="${file##*/}"  # Bỏ phần đường dẫn
+        filename="${filename%.conf}"  # Bỏ phần đuôi .conf
+        listen_directive=$(awk '/^server\s*{/,/^}/ {if ($1 == "listen") print $2}' "$file")
+        if grep -q "ssl_certificate" $file; then
+            echo "$filename Đã cấu hình SSL"
+        else
+            echo "CHUA CAU HINH $filename"
+            sed -i "s/$listen_directive/443 ssl/g" "$file"
+            add_line_config $file
+        fi
+    done
 }
 update_config
-
 
